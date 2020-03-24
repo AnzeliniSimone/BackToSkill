e3bff26f0bdb87b5e0697a66817e61e7e4456daf
frankdf97 <62098049+frankdf97@users.noreply.github.com>
2020-03-24 12:26:05 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,11 +2,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy import create_engine
 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import Table
+from sqlalchemy.orm import backref
+
 #bisogna ancora creare la sessione
 
-engine = create_engine('sqlite:///testdb.db') #non so che path bisogna usare
+engine = create_engine('sqlite:///testdb.db')
 
-Base= declarative_base()
+Base = declarative_base()
+Base.metadata.create_all(engine)
+
+
+
 
 class User(Base):
     __tablename__='user'
@@ -21,7 +30,7 @@ class User(Base):
 class employee(Base):
 
     #Personal
-    _tablename_ = 'employee'
+    _tablename_ = 'employees'
     id = Column(Integer, primary_key = True)
     Photo = ''  #links to be added
     Name_and_Surname = Column(String)
@@ -38,28 +47,28 @@ class Role(Base):
     __tablename__='role'
     id=Column(Integer,primary_key=True)
     name=Column(String)
-    description=Column(string)
+    description=Column(String)
 
 
-employee_softskill=table('employee_softskill',
+employee_softskill= Table('employee_softskill',
                          Base.metadata,
                          Column('employee',Integer,ForeignKey(employee.id)),
                          Column('softskill',Integer,ForeignKey(softskills.id)),
                          Column('grade',Integer))
 #tabella intermedia che collega many to many employees e softskills
 
-employee_hardskill=table('employee_hardskill',Base.metadata,
+employee_hardskill=Table('employee_hardskill',Base.metadata,
                          Column('employee',Integer,ForeignKey(employee.id)),
-                         Column('hardskill',Integer,ForeignKey(hardskills.id)),
+                         Column('hardskill',Integer,ForeignKey(hardskill.id)),
                          Column('grade',Integer))
-#tabella che collega many to many employees e hardskills 
+#tabella che collega many to many employees e hardskills
 
-softskill_role=table('softskill_role',Base.metadata,
+softskill_role=Table('softskill_role',Base.metadata,
                      Column('softskill',Integer,ForeignKey(softskills.id)),
                      Column('role',Integer,ForeignKey(role.id)),
                      Column('grade_request',Integer))
 
-hardskill_role=table('hardskill_role',Base.metadata,
+hardskill_role=Table('hardskill_role',Base.metadata,
                      Column('hardskill',Integer,ForeignKey(hardskills.id)),
                      Column('role',Integer,ForeignKey(role.id)),
                      Column('grade_request',Integer))
@@ -68,40 +77,39 @@ class Soft_skill(Base):
     __tablename__='softskills'
     id=Column(Integer,primary_key=True)
     description=Column(String)
-    employee = Relationship('Employee',secondary=employee_softskill,backref=backref('softskills'))
-    role=Relationship('Role',secondary=softskill_role,backref=backref('softskills'))
+    employee = relationship('Employee',secondary=employee_softskill,backref=backref('softskills'))
+    role= relationship('Role',secondary=softskill_role,backref=backref('softskills'))
 
 class Hard_skill(Base):
     __tablename__='hardskills'
-    id=Column(Integer,primary_key=True)
-    description=Column(String)
-    employee= Relationship ('Employee',secondary=employee_hardskill,backref=backref('hardskills'))
-    role = Relationship('Role', secondary=hardskill_role, backref=backref('hardskills'))
+    id=Column(Integer,primary_key= True)
+    description= Column(String)
+    employee= relationship ('Employee',secondary=employee_hardskill,backref=backref('hardskills'))
+    role = relationship('Role', secondary=hardskill_role, backref=backref('hardskills'))
 
 class education(Base):
     _tablename_ = 'education'
     id = Column(Integer, primary_key=True)
-    employee_id=Column(Integer,ForeignKey(employee.id))
     Degree = Column(String)
     Language_Certification = Column(String)
     Course_Certification = Column(String)
 
 
-employee_trainings= table('employee_trainings',Base.metadata,
+employee_trainings= Table('employee_trainings',Base.metadata,
                          Column('employee',Integer,ForeignKey(employee.id)),
                          Column('trainings',Integer,ForeignKey(trainings.id))
                          )
 #tabella che collega many to many employees e trainings
 
 
-employee_assessment = table('employee_assessment', Base.metadata,
+employee_assessment = Table('employee_assessment', Base.metadata,
                             Column('employee', Integer, ForeignKey(employee.id)),
                             Column('assessment', Integer, ForeignKey(assessment.id))
                             )
 # tabella che collega many to many employees e assessment
 
 
-employee_projects = table('employee_projects', Base.metadata,
+employee_projects = Table('employee_projects', Base.metadata,
                           Column('employee', Integer, ForeignKey(employee.id)),
                           Column('projects', Integer, ForeignKey(projects.id)),
                           )
@@ -115,7 +123,7 @@ class trainings(Base):
     Starting_Date = Column(String)
     Ending_Date = Column(String)
     Skill_Acquired = Column(String)
-    employee = Relationship('employee', secondary=employee_trainings, backref=backref('trainings'))
+    employee = relationship('employee', secondary=employee_trainings, backref=backref('trainings'))
 
 
 class assessment(Base):
@@ -124,7 +132,7 @@ class assessment(Base):
     Assessment = Column(String)
     Date = Column(String)
     Made_By = Column(String)
-    employee = Relationship('employee', secondary=employee_assessment, backref=backref('assessment'))
+    employee = relationship('employee', secondary=employee_assessment, backref=backref('assessment'))
 
 
 class projects(Base):
@@ -133,25 +141,22 @@ class projects(Base):
     Project = Column(String)
     Starting_Date = Column(String)
     Ending_Date = Column(String)
-    employee = Relationship('employee', secondary=employee_projects, backref=backref('projects'))
+    employee = relationship('employee', secondary=employee_projects, backref=backref('projects'))
 
-project_roleinaproject=table('project_role',Base.metadata,
+project_roleinaproject = Table('project_role',Base.metadata,
                              Column('project',Integer,ForeignKey(projects.id)),
-                             Column('roleinproject',Integer,ForeignKey(roleinproject.id)),
+                             Column('roleinproject',Integer,ForeignKey(roleinaproject.id)),
                              Column('numberofpeople',Integer))
 
-class RoleinProject(Base):
-    __tablename__='roleinproject'
+
+class roleinaproject(Base):
+    __tablename__='roleinaproject'
     id=Column(Integer,primary_key=True)
     name=Column(String)
     description=Column(String)
-    project=Relationship('project',secondary=project_roleinaproject,backref=backref('roleinproject'))
-
-Base.metadata.create_all(engine)
+    project= relationship('project',secondary=project_roleinaproject,backref=backref('roleinaproject'))
 
 
-
-    
 
 
 
