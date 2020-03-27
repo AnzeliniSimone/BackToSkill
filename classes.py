939db8dbd0586b16b6518ca16f3980d76c72e892
checcozzo <62096981+checcozzo@users.noreply.github.com>
2020-03-27 16:34:22 +0100

    create classes.py

---
+++ classes.py
@@ -0,0 +1,8 @@
+#prova del file su cui creeremo le classi
+
+class User():
+     def __init__(self,username,password,id):
+         self.username=username
+         self.password=password
+         self.id=id
+
