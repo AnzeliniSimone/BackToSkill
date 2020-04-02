d0ee0edaed8ab2a89f5b80a4529055e317b7d7b4
checcozzo <62096981+checcozzo@users.noreply.github.com>
2020-04-02 17:31:46 +0200

    delete classes.py che non seriviva

--- classes.py
+++
@@ -1,8 +0,0 @@
-#prova del file su cui creeremo le classi
-
-class User():
-     def __init__(self,username,password,id):
-         self.username=username
-         self.password=password
-         self.id=id
-
